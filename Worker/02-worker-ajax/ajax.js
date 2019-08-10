3162e6357dd3e5902bc070d41ec59970088e89c7
keiing <49868852+keiing@users.noreply.github.com>
2019-08-11 00:54:33 +0800

    worker-ajax

--- Worker/02-worker-ajax/ajax.js
+++ Worker/02-worker-ajax/ajax.js
@@ -2,21 +2,34 @@
     window.addEventListener('message', function (e) {
         this.console.log(e.data);
         var _this = this;
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300 || xhr.readyState === 4 && xhr.status === 304) {
-                var result = JSON.parse(xhr.responseText);
-                if (result !== undefined) {
-                    // resolve(result);
-                    _this.postMessage(result);
-                    //成功发送消息之后调用结束 之后关闭线程
-                    _this.close();
+        var {
+            url,
+            type,
+            data,
+            dataType
+        } = e.data
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300 || xhr.readyState === 4 && xhr.status === 304) {
+                    if (dataType !== undefined &&dataType.toLowerCase() === "json")var result = JSON.parse(xhr.responseText)
+                    else var result = xhr.responseText
+                    _this.postMessage((result))
                 }
             }
-        }
-        //127.242.19.39
-        xhr.open('get', e.data, true);
-        xhr.send();
+            if (type.toLowerCase() == "get" && data != undefined) {
+                url += "?" + data;
+            }
+            //3.打开连接
+            xhr.open(type, url, true);
+            if (type.toLowerCase() === "post")
+                //增加：设置请求消息头
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            //4.发送请求
+            if (type.toLowerCase() == "post" && data !== undefined)
+                xhr.send(data);
+            else
+                xhr.send(null);
+
     });
     window.addEventListener('messageerror', function (e) {
         // 指定 messageerror 事件的监听函数。发送的数据无法序列化成字符串时，会触发这个事件。
