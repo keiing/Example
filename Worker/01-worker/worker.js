1a45aa5de78ff1d366d5509657dd866d81d094a1
keiing <49868852+keiing@users.noreply.github.com>
2019-08-12 22:08:36 +0800

    worker

--- Worker/01-worker/worker.js
+++ Worker/01-worker/worker.js
@@ -14,7 +14,6 @@
     }
     //写法二 意思相同
     // addEventListener('message',function(e){this.postMessage('my)})
-
     window.addEventListener('messageerror', function (err) {
         // 指定 messageerror 事件的监听函数。 发送的数据无法序列化成字符串时， 会触发这个事件。
         window.close();//关闭Worker线程
