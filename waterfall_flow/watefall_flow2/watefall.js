7ea557f064da458808a995ce1abb0d2677989476
keiing <49868852+keiing@users.noreply.github.com>
2019-08-16 20:01:04 +0800

    watefall

--- waterfall_flow/watefall_flow2/watefall.js
+++ waterfall_flow/watefall_flow2/watefall.js
@@ -6,7 +6,10 @@
 function watefall(areaDom,urls,everyWidth){
     //创建文本节点
     var div=document.createDocumentFragment();
-    createImageDoms(console.log(urls));
+    //1.创建dom对象
+    createImageDoms();
+    //2.设置每张图片坐标
+    setImgPosition();
     //函数区
     /** 
      * 创建图片的dom对象
@@ -16,9 +19,13 @@ function watefall(areaDom,urls,everyWidth){
            let url=urls[i];
            var img=document.createElement('img');
            img.src="http://127.0.0.1:3000/"+url.image;
-           img.setAttribute('style',`width:${everyWidth}px`)
+           img.setAttribute('style',`width:${everyWidth}px;position:absolute;`)
            div.appendChild(img)
        }
        areaDom.appendChild(div)
    }
+   /** 
+    * 设置每张图片的坐标
+   */
+   function setImgPosition(){}
 }
